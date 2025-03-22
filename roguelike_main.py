4a16a13b1f818c6b309cc36f96f27a8e87ea2fe7
Logan John Owens <loganjohnowens@users.noreply.github.com>
2025-03-22 19:20:41 -0400

    added_doors

--- roguelike_main.py
+++ roguelike_main.py
@@ -7,7 +7,7 @@ def screen():
     global canvas
     pygame.init()
     canvas = pygame.display.set_mode((500, 500))
-    pygame.display.set_caption("square game")
+    pygame.display.set_caption("roguelike")
 
 
 def walls(wide, high, x, y):
@@ -70,8 +70,8 @@ def random_square():
     global long
     global pos_tall
     global pos_long
-    tall = random.randint(100, 200)
-    long = random.randint(100, 200)
+    tall = random.randint(200, 300)
+    long = random.randint(200, 300)
     pos_tall = (500 - tall) / 2
     pos_long = (500 - long) / 2
 
@@ -88,8 +88,51 @@ def doors():
     global long
     global pos_tall
     global pos_long
+    global new_room
+    global player_size
+    global pos
+    rect2 = pygame.Rect(pos[0], pos[1], player_size[0], player_size[1])
     pygame.draw.rect(canvas, (255, 50, 0),
-                     pygame.Rect(pos_long, pos_tall, long, tall))
+                     pygame.Rect(pos_long + long / 2, pos_tall + 20, 20, 10))
+    rect1 = pygame.Rect(pos_long + long / 2, pos_tall + 20, 20, 10)
+    if rect1.colliderect(rect2):
+        new_room = True
+    pygame.draw.rect(canvas, (255, 50, 0),
+                     pygame.Rect(pos_long + long / 2, pos_tall + tall - 10, 20, 10))
+    rect1 = pygame.Rect(pos_long + long / 2, pos_tall + tall - 10, 20, 10)
+    if rect1.colliderect(rect2):
+        new_room = True
+    pygame.draw.rect(canvas, (255, 50, 0),
+                     pygame.Rect(pos_long + 20, pos_tall + tall / 2, 10, 20))
+    rect1 = pygame.Rect(pos_long + 20, pos_tall + tall / 2, 10, 20)
+    if rect1.colliderect(rect2):
+        new_room = True
+    pygame.draw.rect(canvas, (255, 50, 0),
+                     pygame.Rect(pos_long + long - 10, pos_tall + tall / 2, 10, 20))
+    rect1 = pygame.Rect(pos_long + long - 10, pos_tall + tall / 2, 10, 20)
+    if rect1.colliderect(rect2):
+        new_room = True
+    if new_room is True:
+        random_square()
+        pos = [250, 250]
+        new_room = False
+
+
+def dev_tools():
+    global dev_mode
+    global direct
+    global loop
+    global new_room
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_l]:
+        dev_mode = True
+    if dev_mode:
+        if keys[pygame.K_LSHIFT]:
+            direct = [True, True, True, True]
+        if keys[pygame.K_k]:
+            loop = False
+        if keys[pygame.K_r]:
+            new_room = True
 
 
 def main():
@@ -98,12 +141,18 @@ def main():
     global direct
     global player_size
     global first
+    global dev_mode
+    global loop
+    global new_room
+    new_room = False
+    loop = True
+    dev_mode = False
     first = True
     direct = [True, True, True, True]
     pos = [250, 250]
     player_size = [10, 10]
     random_square()
-    while True:
+    while loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -112,6 +161,8 @@ def main():
         player_move()
         player()
         display_random_square()
+        dev_tools()
+        doors()
         update_screen()
         first = True
 
